1ae2e756096328ed7b971e3cb4d42f243ba5cc17
Remi IO <i@remi.email>
2021-11-04 09:46:33 +0000

    save works. (try myalgo wallet api)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,27 @@ import { useCallback, useState } from "react";
 import MyAlgoConnect from '@randlabs/myalgo-connect';
 import algosdk from "algosdk";
 
+const myAlgoConnect = new MyAlgoConnect();
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
 
 function App() {
 
   const [algoUserAddresses, setAlgoUserAddresses] = useState<any[]>([]);
 
   const getAlgoUserAddress = useCallback(async () => {
-    const myAlgoConnect = new MyAlgoConnect();
+
     const accountsSharedByUser = await myAlgoConnect.connect()
     console.log(accountsSharedByUser);
     setAlgoUserAddresses(accountsSharedByUser);
   }, [algoUserAddresses])
+
+  const signMessage = useCallback(async () => {
+    const encodeMessage = encoder.encode('fff');
+    const msg = await myAlgoConnect.signLogicSig(encodeMessage, algoUserAddresses[0].address);
+    console.log(msg);
+    console.log(decoder.decode(msg));
+  }, [algoUserAddresses]);
 
   return <>
     <div>
@@ -21,7 +31,7 @@ function App() {
     </div>
     <div>
       {algoUserAddresses.length > 0 ? algoUserAddresses.map(i => {
-        return <div>Address:{i.address}</div>
+        return <div>Address:{i.address} <button onClick={() => { signMessage() }}>sign</button></div>
       }) : <button onClick={() => { getAlgoUserAddress() }}>Login with MyAlgo</button>}
 
 
