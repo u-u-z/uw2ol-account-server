820169c6d4e488fb0a34a16cd41f33dd491e74bd
Remi IO <i@remi.email>
2021-11-04 09:08:11 +0000

    Add myalgo-connect get user Address.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,119 +1,33 @@
-import { useCallback, useState } from "react"
-import { Web3Provider } from "@ethersproject/providers"
-import { auth, getNonce, login, register } from "./services"
+import MetaMask from "./components/MetaMask";
+import { useCallback, useState } from "react";
+import MyAlgoConnect from '@randlabs/myalgo-connect';
+import algosdk from "algosdk";
 
-enum State {
-  Login,
-  RegisterOrConfirm,
-  Success,
-}
-
-let accessToken: string | null = null;
 
 function App() {
-  const [state, setState] = useState(State.Login);
-  const [loading, setLoading] = useState(false);
-  const [name, setName] = useState<string>("");
 
-  const onAuth = useCallback(async () => {
-    if (!(window as any).ethereum) {
-      window.alert("Please install MetaMask first.");
-      return;
-    }
+  const [algoUserAddresses, setAlgoUserAddresses] = useState<any[]>([]);
 
-    const session = new URLSearchParams(window.location.search).get("session");
+  const getAlgoUserAddress = useCallback(async () => {
+    const myAlgoConnect = new MyAlgoConnect();
+    const accountsSharedByUser = await myAlgoConnect.connect()
+    console.log(accountsSharedByUser);
+    setAlgoUserAddresses(accountsSharedByUser);
+  }, [algoUserAddresses])
 
-    if (!session) {
-      window.alert("Missing session");
-      return;
-    }
-
-    setLoading(true);
-
-    try {
-      await (window as any).ethereum.request({ method: "eth_requestAccounts" });
-
-      const provider = new Web3Provider((window as any).ethereum);
-      const signer = provider.getSigner();
-
-      const address = await signer.getAddress();
-      const nonce = await getNonce(address);
-      const signature = await signer.signMessage(`I'm signing my one-time nonce: ${nonce}`);
-
-      const authResponse = await auth(address, signature, session);
-
-      accessToken = authResponse.accessToken;
-      setName(authResponse.name ?? "");
-      setState(State.RegisterOrConfirm);
-    } finally {
-      setLoading(false);
-    }
-  }, [setLoading, setName, setState]);
-
-  const onLogin = useCallback(async () => {
-    try {
-      setLoading(true);
-      await login(accessToken!);
-      setState(State.Success);
-    } finally {
-      setLoading(false);
-    }
-  }, [setLoading, setState]);
-
-  const [inputName, setInputName] = useState("");
-
-  const onInputNameChange = useCallback((e: React.FormEvent<HTMLInputElement>) => {
-    setInputName(e.currentTarget.value);
-  }, [setInputName]);
-
-  const onRegister = useCallback(async () => {
-    try {
-      setLoading(true);
-      await register(accessToken!, inputName);
-      setState(State.Success);
-    } finally {
-      setLoading(false);
-    }
-  }, [setLoading, setState, inputName]);
-
-  if (state === State.Login) {
-    return (
-      <div>
-        <button onClick={onAuth} disabled={loading}>
-          {loading ? "Loading..." : "Login with Metamask"}
-        </button>
-      </div>
-    );
-  }
-
-  if (state === State.RegisterOrConfirm) {
-    if (name.length > 0) {
-      return (
-        <div>
-          <span>Your character name: {name}</span>
-          <button onClick={onLogin} disabled={loading}>
-            {loading ? "Loading..." : "Confirm"}
-          </button>
-        </div>
-      );
-    }
-
-    return (
-      <div>
-        <span>Please input your name:</span>
-        <input type="text" minLength={3} value={inputName} onChange={onInputNameChange} />
-          <button onClick={onRegister} disabled={loading}>
-            {loading ? "Loading..." : "Confirm"}
-          </button>
-      </div>
-    );
-  }
-
-  return (
+  return <>
     <div>
-      Success
+      <MetaMask />
     </div>
-  );
+    <div>
+      {algoUserAddresses.length > 0 ? algoUserAddresses.map(i => {
+        return <div>Address:{i.address}</div>
+      }) : <button onClick={() => { getAlgoUserAddress() }}>Login with MyAlgo</button>}
+
+
+
+    </div>
+  </>
 }
 
 export default App;
