0bad4904cd30b8b56a554b810099363cdb241e5b
Remi IO <i@remi.email>
2021-11-04 12:19:38 +0000

    save works.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,7 @@ import MyAlgoConnect from '@randlabs/myalgo-connect';
 import algosdk from "algosdk";
 
 const myAlgoConnect = new MyAlgoConnect();
-const encoder = new TextEncoder();
-const decoder = new TextDecoder();
+
 
 function App() {
 
@@ -18,11 +17,19 @@ function App() {
     setAlgoUserAddresses(accountsSharedByUser);
   }, [algoUserAddresses])
 
-  const signMessage = useCallback(async () => {
-    const encodeMessage = encoder.encode('fff');
-    const msg = await myAlgoConnect.signLogicSig(encodeMessage, algoUserAddresses[0].address);
-    console.log(msg);
-    console.log(decoder.decode(msg));
+  const signMessage = useCallback(async (index: number) => {
+    const algodClient = new algosdk.Algodv2("", 'https://api.testnet.algoexplorer.io', '');
+    const params = await algodClient.getTransactionParams().do();
+    const txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+      suggestedParams: {
+        ...params,
+      },
+      from: algoUserAddresses[index].address,
+      to: algoUserAddresses[index].address,
+      amount: 7788
+    });
+    const signedTxn = await myAlgoConnect.signTransaction(txn.toByte());
+    console.log(signedTxn);
   }, [algoUserAddresses]);
 
   return <>
@@ -30,8 +37,8 @@ function App() {
       <MetaMask />
     </div>
     <div>
-      {algoUserAddresses.length > 0 ? algoUserAddresses.map(i => {
-        return <div>Address:{i.address} <button onClick={() => { signMessage() }}>sign</button></div>
+      {algoUserAddresses.length > 0 ? algoUserAddresses.map((i, key) => {
+        return <div>Address:{i.address} <button onClick={() => { signMessage(key) }}>sign</button></div>
       }) : <button onClick={() => { getAlgoUserAddress() }}>Login with MyAlgo</button>}
 
 
